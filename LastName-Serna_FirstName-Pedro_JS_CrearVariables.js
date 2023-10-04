8cb8cb6093eeefc90c91b70078a98e29b6b710e7
Pedro Serna <pserna364@gmail.com>
2023-10-04 01:33:31 -0300

    SubeteaeseJuego
    Se modifica el archivo de variables para la segunda actividad de súbete a ese juego.

--- LastName-Serna_FirstName-Pedro_JS_CrearVariables.js
+++ LastName-Serna_FirstName-Pedro_JS_CrearVariables.js
@@ -1,38 +1,59 @@
-/* PASO 1:IDENTIFICAR VARIABLES
-    Para determinar si una persona puede subir a la Montaña Rusa se necesitan dos variables numéricas:
-    1- Una para almacenar la altura.
-    2- Otra para almacenar la edad. 
-    3- (Opcional)Otra también para los datos de la persona, como el nombre y apellido para tener un registro, 
-    aunque esta realmente no condicionaría la entrada a la Montaña Rusa.
+/* PEDRO SERNA, LINK DE GIT: https://github.com/pedroserna22/crearVariables
+Hay una branch con la solución de esta actividad, el main contiene la actividad de crear variables*/
 
- PASO 2:CREACIÓN DE VARIABLES
-    DUDA: En el material se usa para crear variables "var", usualmente uso "let" 
-    tengo entendido que tienen algunas diferencias, pero no sé cuál es mas recomendable */
-
-var firstName;
-var lastName;//Como dije mas arriba, estas dos variables no cambian el resultado, lo que determina si una persona sube es la edad y la altura.
-
-var alturadePersona;
-var edaddePersona;
-
-/* PASO 3:EXPLICACIÓN
-    Para que una persona pueda subirse, es necesario que cumplan las condiciones dadas por el ejercicio
-    Que como bien dice, "deben medir al menos un metro" y "ser mayor a 10 años"
-    Con otra sintaxis debería ser: alturadePersona >= 1 & edaddePersona > 10*/
+/* PASO 1:REVISAR Y EDITAR VARIABLES*/
+/* VARIABLES DEFINIDAS ANTERIORMENTE
 firstName= Pedro;
 lastName= Serna;
-alturadePersona = 1.3;//Cumple ya que se exigía 1 metro de altura.
-edaddePersona = 13;//Cumple ya que se exigía 13 años de edad.
+alturadePersona = 1.3; Ahora según la letra tiene que ser mayor a 52, y no a 1 metro como se decía antes.
+edaddePersona = 13; La edad mínima se mantiene en 10
+"(alturadePersona >= 1 Y edaddePersona > 10)"
+*/
 
-/* En el paso 2 las variables se declaran, en el paso 3 se les asigna un valor, en este último
-   punto AMBAS VARIABLES cumplen con los requisitos para subir a la Montaña Rusa.
-   Sin embargo, pasando a como es que se comprueba si la persona cumple esas condiciones
-   en código o mejor dicho en pseudocódigo, sería aplicando un condicional if else, es decir SI cumple tal condicion SINO ... 
+/*REDECLARACIÓN DE VARIABLES Y CREACIÓN DE FUNCIÓN
+La edad mínima en 10 y la altura se define en 52.
+En esta función solamente se comprueba la altura, toma como parámetro una altura y comprueba en el if si 
+es mayor a la altura mínima, es decir que cumple con la condición, entonces
+retornará a "¡Súbete, chico!", sino de lo contrario a "Lo siento, chico. Tal vez el próximo año."*/
 
-   SI (alturadePersona >= 1 Y edaddePersona > 10) Entonces
-        Escribir "Esta persona puede subir a la Montaña"
-    SINO
-        Escribir "Esta persona no está habilitada a subir"
- */
+alturaMin=52;
+edadMin=10;
 
+function subealaMontañaRusa(altura) {
+    if (altura > alturaMin) {
+      return "¡Súbete, chico!";
+    } else {
+      return "Lo siento, chico. Tal vez el próximo año.";
+    }
+  }
 
+/*STRETCH 1
+Se modifica la función para que a parte de requerir y comprobar la altura haga lo mismo con la edad
+Se hace uso del operador AND "&&", en el caso que ambas condiciones se cumplan entra en el primer return, sino en el siguiente.*/  
+function subealaMontañaRusaStretch1(altura, edad) {
+    if (altura > alturaMin && edad > edadMin) {
+      return "¡Súbete, chico!";
+    } else {
+      return "Lo siento, chico. Tal vez el próximo año.";
+    }
+  }
+
+/*STRETCH 2
+Para el stretch 2 se modifica nuevamente la función, sin embargo la estructura y lógica es la misma, simplemente
+se cambia el operador en el condicional pasando a ser OR, es decir, si se cumple una de las dos condiciones
+el niño podrá subir a la montaña, si no se cumple ninguna, no.*/  
+function subealaMontañaRusaStretch2(altura, edad) {
+    if (altura > alturaMin || edad > edadMin) {
+      return "¡Súbete, chico!";
+    } else {
+      return "Lo siento, chico. Tal vez el próximo año.";
+    }
+  }
+
+/*PRUEBAS STRETCH 2*/
+console.log(subealaMontañaRusaStretch2(60,11))//Cumple las DOS condiciones, por lo que entrará en el primer return y mostrará "¡Súbete, chico!".
+console.log(subealaMontañaRusaStretch2(60,9))//Cumple las UNA condición por lo que entrará en el primer return y mostrará "¡Súbete, chico!".
+console.log(subealaMontañaRusaStretch2(50,9))//No cumple ninguna condición así que mostrará "Lo siento, chico. Tal vez el próximo año.".
+/*PRUEBAS STRETCH 1*/
+console.log(subealaMontañaRusaStretch1(60,10))//Se cumple UNA condición, pero se necesitan las dos ya que el operador es AND asi que mostrará "Lo siento, chico. Tal vez el próximo año.".
+console.log(subealaMontañaRusaStretch1(58,13))//Cumple las DOS condiciones, por lo que entrará en el primer return y mostrará "¡Súbete, chico!".
