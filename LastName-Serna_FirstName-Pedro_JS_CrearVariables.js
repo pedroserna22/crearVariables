a5b4469041aaafb6ff0feced833e05bc2130df5e
Pedro Serna <pserna364@gmail.com>
2023-10-05 21:43:34 -0300

    Solución al desafío Súbete a ese juego
    Solución al desafío Súbete a ese juego.

--- LastName-Serna_FirstName-Pedro_JS_CrearVariables.js
+++ LastName-Serna_FirstName-Pedro_JS_CrearVariables.js
@@ -14,27 +14,27 @@ edaddePersona = 13; La edad mínima se mantiene en 10
 La edad mínima en 10 y la altura se define en 52.
 En esta función solamente se comprueba la altura, toma como parámetro una altura y comprueba en el if si 
 es mayor a la altura mínima, es decir que cumple con la condición, entonces
-retornará a "¡Súbete, chico!", sino de lo contrario a "Lo siento, chico. Tal vez el próximo año."*/
+imprime por consola "¡Súbete, chico!", sino de lo contrario a "Lo siento, chico. Tal vez el próximo año."*/
 
 alturaMin=52;
 edadMin=10;
 
 function subealaMontañaRusa(altura) {
     if (altura > alturaMin) {
-      return "¡Súbete, chico!";
+      console.log("¡Súbete, chico!");
     } else {
-      return "Lo siento, chico. Tal vez el próximo año.";
+      console.log("Lo siento, chico. Tal vez el próximo año.");
     }
   }
 
 /*STRETCH 1
 Se modifica la función para que a parte de requerir y comprobar la altura haga lo mismo con la edad
-Se hace uso del operador AND "&&", en el caso que ambas condiciones se cumplan entra en el primer return, sino en el siguiente.*/  
+Se hace uso del operador AND "&&", en el caso que ambas condiciones se cumplan entra se imprime "¡Súbete, chico!".*/  
 function subealaMontañaRusaStretch1(altura, edad) {
     if (altura > alturaMin && edad > edadMin) {
-      return "¡Súbete, chico!";
+      console.log("¡Súbete, chico!");
     } else {
-      return "Lo siento, chico. Tal vez el próximo año.";
+      console.log("Lo siento, chico. Tal vez el próximo año.");
     }
   }
 
@@ -44,16 +44,16 @@ se cambia el operador en el condicional pasando a ser OR, es decir, si se cumple
 el niño podrá subir a la montaña, si no se cumple ninguna, no.*/  
 function subealaMontañaRusaStretch2(altura, edad) {
     if (altura > alturaMin || edad > edadMin) {
-      return "¡Súbete, chico!";
+      console.log("¡Súbete, chico!");
     } else {
-      return "Lo siento, chico. Tal vez el próximo año.";
+      console.log("Lo siento, chico. Tal vez el próximo año.");
     }
   }
 
 /*PRUEBAS STRETCH 2*/
-console.log(subealaMontañaRusaStretch2(60,11))//Cumple las DOS condiciones, por lo que entrará en el primer return y mostrará "¡Súbete, chico!".
-console.log(subealaMontañaRusaStretch2(60,9))//Cumple las UNA condición por lo que entrará en el primer return y mostrará "¡Súbete, chico!".
-console.log(subealaMontañaRusaStretch2(50,9))//No cumple ninguna condición así que mostrará "Lo siento, chico. Tal vez el próximo año.".
+subealaMontañaRusaStretch2(60,11)//Cumple las DOS condiciones, por lo que imprime "¡Súbete, chico!".
+subealaMontañaRusaStretch2(60,9)//Cumple UNA condición por lo que imprime "¡Súbete, chico!".
+subealaMontañaRusaStretch2(50,9)//No cumple ninguna condición así que mostrará "Lo siento, chico. Tal vez el próximo año.".
 /*PRUEBAS STRETCH 1*/
-console.log(subealaMontañaRusaStretch1(60,10))//Se cumple UNA condición, pero se necesitan las dos ya que el operador es AND asi que mostrará "Lo siento, chico. Tal vez el próximo año.".
-console.log(subealaMontañaRusaStretch1(58,13))//Cumple las DOS condiciones, por lo que entrará en el primer return y mostrará "¡Súbete, chico!".
+subealaMontañaRusaStretch1(60,10)//Se cumple UNA condición, pero se necesitan las dos ya que el operador es AND asi que mostrará "Lo siento, chico. Tal vez el próximo año.".
+subealaMontañaRusaStretch1(58,13)//Cumple las DOS condiciones, por lo que imprime "¡Súbete, chico!".
